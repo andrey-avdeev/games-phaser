f901a15a32685f06a127004d64017e1694377996
andreyavdeev <andreyavdeev@RUMSKR90AZ63B>
2017-03-26 22:39:33 +0300

    stubs

--- Farm/src/Main.ts
+++ Farm/src/Main.ts
@@ -4,13 +4,41 @@ namespace Farm {
 
         public background: Phaser.Sprite;
 
-        create() {
-            this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        public chiken: Phaser.Sprite;
+        public horse: Phaser.Sprite;
+        public pig: Phaser.Sprite;
+        public sheep: Phaser.Sprite;
 
+        public rightArrow: Phaser.Sprite;
+        public leftArrow: Phaser.Sprite;
+
+        init() {
+            //scalling screen
+            this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
             this.scale.pageAlignHorizontally = true;
             this.scale.pageAlignVertically = true;
+        }
 
+        create() {
             this.background = this.game.add.sprite(0, 0, "background");
+
+            this.chiken = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY / 2, 'chiken');
+            this.chiken.anchor.setTo(0.5);
+            this.chiken.scale.setTo(1.5);
+
+            this.horse = this.game.add.sprite(100, 200, 'horse');
+            this.horse.anchor.setTo(0.5);
+
+            this.pig = this.game.add.sprite(150, 100, 'pig');
+            this.pig.anchor.setTo(0.5);
+
+            this.sheep = this.game.add.sprite(100, 100, 'sheep');
+            this.sheep.anchor.setTo(0.5);
+
+            this.rightArrow = this.game.add.sprite(this.game.width - 100, this.game.world.centerY, 'arrow');
+            this.leftArrow = this.game.add.sprite(100, this.game.world.centerY, 'arrow');
+            this.leftArrow.scale.setTo(-1, 0);
+
         }
     }
 }
--- Farm/src/Preloader.ts
+++ Farm/src/Preloader.ts
@@ -6,12 +6,12 @@ namespace Farm {
             this.load.image("background", "assets/images/background.png");
             this.load.image("arrow", "assets/images/arrow.png");
 
-            this.load.spritesheet("chiken", "assets/images/chiken_spritesheet.png", 131, 200, 3);
+            this.load.spritesheet("chiken", "assets/images/chicken_spritesheet.png", 131, 200, 3);
             this.load.spritesheet("horse", "assets/images/horse_spritesheet.png", 212, 200, 3);
             this.load.spritesheet("pig", "assets/images/pig_spritesheet.png", 297, 200, 3);
             this.load.spritesheet("sheep", "assets/images/sheep_spritesheet.png", 244, 200, 3);
 
-            this.load.audio("chikenSound", ["assets/audio/chiken.ogg", "assets/audio/chiken.mp3"]);
+            this.load.audio("chikenSound", ["assets/audio/chiken.ogg", "assets/audio/chicken.mp3"]);
             this.load.audio("horseSound", ["assets/audio/horse.ogg", "assets/audio/horse.mp3"]);
             this.load.audio("pigSound", ["assets/audio/pig.ogg", "assets/audio/pig.mp3"]);
             this.load.audio("sheepSound", ["assets/audio/sheep.ogg", "assets/audio/sheep.mp3"]);
